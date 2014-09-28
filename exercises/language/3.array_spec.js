b78b86d54e23baf16c412a6903b8058334f993f6
Jean Carlos Meninno <jean-carlos.meninno@xing.com>
2014-09-28 12:21:00 +0200

    Added working with arrays basics

---
+++ exercises/language/3.array_spec.js
@@ -0,0 +1,71 @@
+var assert = require('chai').assert;
+
+describe('Working with Arrays', function() {
+
+  // Change the expresion after the comma to fullfill the expectation
+  // The `assert.equals` expresion makes the test pass if and only if both
+  // expresions are equal.
+  // General form:
+  // assert.equal(<expression>, <expressionExpectation>);
+
+  it('create an array without the new operator', function() {
+    var array;
+
+    assert.isArray(array);
+  })
+
+  it('calculates the numbers of item in an array', function() {
+    var fruits = ['orange', 'apple', 'banana', 'pineapple'];
+    var length;
+
+    assert.equal(4, length);
+  })
+
+  it('adds an element to the begining of the array', function() {
+    var fruits = ['apple', 'banana', 'pineapple'];
+    var orange = 'orange';
+
+    assert.deepEqual(['orange', 'apple', 'banana', 'pineapple'], fruits)
+  })
+
+  it('adds an element to the end of the array', function() {
+    var fruits    = ['orange', 'apple', 'banana'];
+    var pineapple = 'pineapple';
+
+    assert.deepEqual(['orange', 'apple', 'banana', 'pineapple'], fruits)
+  })
+
+  it('remove an element at the begining of the array', function() {
+    var fruits    = ['orange', 'apple', 'banana', 'pineapple'];
+
+    assert.deepEqual(['apple', 'banana', 'pineapple'], fruits)
+  })
+
+  it('remove an element at the end of the array', function() {
+    var fruits = ['orange', 'apple', 'banana', 'pineapple'];
+
+    assert.deepEqual(['orange', 'apple', 'banana'], fruits)
+  })
+
+  it('equality', function() {
+    var fruits1 = ['orange', 'apple', 'banana', 'pineapple'];
+    var fruits2 = ['orange', 'apple', 'banana', 'pineapple'];
+    var equality = fruits1 == fruits2;
+
+    assert.equal(equality, true)
+  })
+
+  it('equality with type checking', function() {
+    var fruits1 = ['orange', 'apple', 'banana', 'pineapple'];
+    var fruits2 = ['orange', 'apple', 'banana', 'pineapple'];
+    var equality = fruits1 === fruits2;
+
+    assert.equal(equality, true)
+  })
+
+  it('creates a string from an array', function() {
+    var fruits = ['orange', 'apple', 'banana', 'pineapple'];
+
+    assert.equal('orange, apple, banana, pineapple', string)
+  })
+})
