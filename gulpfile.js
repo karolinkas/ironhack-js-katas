5f583fbf8a799d5643d524c8220bd785860eaac7
Jean Carlos Meninno <jean-carlos.meninno@xing.com>
2014-09-29 01:18:29 +0200

    Update gulp tasks to use mocha-phantom

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
     //Gulp and plugins
 var gulp        = require('gulp'),
-    mocha       = require('gulp-mocha'),
+    concat      = require('gulp-concat'),
     plumber     = require('gulp-plumber'),
-    watch       = require('gulp-watch');
+    watch       = require('gulp-watch'),
+    runner      = require('gulp-mocha-phantomjs');
 
-gulp.task('exercises:language:runner', function() {
-  gulp.src('exercises/language/*.js', {read: false})
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec', bail: true}));
-});
 
 gulp.task('exercises:language:watcher', function() {
-  gulp.watch('exercises/language/*.js', ['exercises:language:runner']);
+  gulp.watch('exercises/language/*_spec.js', ['exercises:language:builder', 'exercises:language:runner']);
+});
+
+gulp.task('exercises:language:builder', function() {
+  gulp.src(['exercises/language/*.js'])
+    .pipe(plumber())
+    .pipe(concat('tests.js'))
+    .pipe(gulp.dest('exercises/test_runner'))
+});
+
+
+gulp.task('exercises:language:runner', function () {
+  gulp.src('exercises/test_runner/index.html')
+    .pipe(plumber())
+    .pipe(runner({mocha:{bail:true}}));
 });
 
 gulp.task('exercises:language', ['exercises:language:watcher']);
